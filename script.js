cbce2877e96f91bd29e5fa19927b8182c8343bbd
MohamedUmar083 <umarmohd777@gmail.com>
2024-04-01 00:18:07 +0530

    Day-7/Tasks

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ request.onload = function () {
   const data = JSON.parse(result);
   //console.log(data)
 
-  /*
-  //task-1
-  const asia = data.filter((reg)=>reg.region==="Asia");
-  asia.map((country)=>console.log(country.name.common))
+/*
+//Task-a
+const asia = data.filter((reg)=>reg.region==="Asia");
+asia.map((country)=>console.log(country.name.common))
 */
 
-  /*
-//task-2
+/*
+//Task-b
 const popul = data.filter((popu)=>popu.population<200000)
 popul.map((pname)=>console.log(pname.name.common));
 */
 
-  /*
-//task-3
+/*
+//Task-c
 
 const t3 = data.forEach((element )=>console.log(`Name : ${element.name.common}, Capital : ${element.capital[0]}, Flag : ${element.flag}`));
 */
 
-  /*
-//task-4
+/*
+//Task-d
 
 const population = data.map((popu)=>popu.population);
 const t4 = population.reduce((sum,i)=>{
@@ -35,9 +35,8 @@ console.log(t4)
 */
 
 
-  //task-5
-
-  const cur = data.filter((curren)=>curren?.currencies?.USD);
-  cur.map((usd)=>console.log(usd.name.common));
+//Task-e
+const cur = data.filter((curren)=>curren?.currencies?.USD);
+cur.map((usd)=>console.log(usd.name.common));
   
 };
