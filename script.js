80440f46ebce5573fe47363495ef53b7805fe63e
MohamedUmar083 <umarmohd777@gmail.com>
2024-03-31 23:01:15 +0530

    Day-7/Tasks

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-const a = [6, 5, 3, 4, 1, 2];
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/all", true);
+request.send();
+request.onload = function () {
+  const result = request.response;
+  const data = JSON.parse(result);
+  //console.log(data)
+
+  /*
+  //task-1
+  const asia = data.filter((reg)=>reg.region==="Asia");
+  asia.map((country)=>console.log(country.name.common))
+*/
+
+  /*
+//task-2
+const popul = data.filter((popu)=>popu.population<200000)
+popul.map((pname)=>console.log(pname.name.common));
+*/
+
+  /*
+//task-3
+
+const t3 = data.forEach((element )=>console.log(`Name : ${element.name.common}, Capital : ${element.capital[0]}, Flag : ${element.flag}`));
+*/
+
+  /*
+//task-4
+
+const population = data.map((popu)=>popu.population);
+const t4 = population.reduce((sum,i)=>{
+  return sum+i;
+},0)
+console.log(t4)
+*/
 
 
-const result = a.filter(function(great){
-    return great>2;
-});
-console.log(result);
+  //task-5
+
+  const cur = data.filter((curren)=>curren?.currencies?.USD);
+  cur.map((usd)=>console.log(usd.name.common));
+  
+};
